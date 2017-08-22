7a96e9b78286dc196e1a4ef8c56730aaa7eeaef3
hungmuha <hung.soboss@gmail.com>
2017-08-22 09:06:23 -0600

    got thru sprint 3

--- starter-code/src/db/seed.ts
+++ starter-code/src/db/seed.ts
@@ -1,15 +1,25 @@
 import { db } from '../models';
 let DB = db.models;
 
-var artistCreate = function() {
-	return DB.Artist.create({
-    name: 'Luciano Pavarotti',
-    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
-    nationality: 'Italiano',
-    instrument: 'Voice',
-    home_address: '1 Strada Roma'
-  });
-};
+var lucySongs = [
+	{
+		title: "O sole mio",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert",
+		artistId: ""
+	},
+	{
+		title: "Nessun dorma",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert",
+		artistId: ""
+	}
+];
+
+
+	
 
 var managerCreate = function() {
 	return DB.Manager.create({
@@ -17,7 +27,28 @@ var managerCreate = function() {
     email: 'rbobby@gmail.com',
     office_number: '516-877-0304',
     cell_phone_number: '718-989-1231'
-	});
+	}).then(function(manager){
+		return DB.Ad.create({
+	  	 headline: 'get me some talent',
+	  	 url:'http://creatv.com',
+	  	 managerId: manager.id
+	  	}),
+		DB.Artist.create({
+	    name: 'Luciano Pavarotti',
+	    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
+	    nationality: 'Italiano',
+	    instrument: 'Voice',
+	    home_address: '1 Strada Roma',
+	    managerId: manager.id
+	  }).then(function(artist) {
+	  	lucySongs.forEach(function(song) {
+	  		song.artistId = artist.id;
+	  	});
+	  	DB.Song.bulkCreate(lucySongs);
+	  }).then(function(manager){
+	  	
+	  });
+	})
 };
 
 var songCreate = function() {
@@ -26,11 +57,11 @@ var songCreate = function() {
 	    duration: '3:31',
 	    date_of_release: '7/13/2015',
 	    album_title: 'Best Album Ever'
-	});
+	},lucySongs);
 };
 
-artistCreate()
-.then(managerCreate)
+
+managerCreate()
 .then(songCreate)
 .then(function() {
 	process.exit();
--- starter-code/src/models/index.ts
+++ starter-code/src/models/index.ts
@@ -1,17 +1,25 @@
 //Connect
 import * as Sequelize from 'sequelize';
 
-var sequelize = new Sequelize('postgres://<username>@localhost:5432/tunr_relationships');
+var sequelize = new Sequelize('postgres://hungmuhamath@localhost:5432/tunr_relationships');
 
 var Artist = sequelize.import("./artist");
 var Manager = sequelize.import("./manager");
-var Song = sequelize.import("./song")
+var Song = sequelize.import("./song");
+var Ad = sequelize.import("./ad");
+
+Song.belongsTo(Artist);
+Artist.hasMany(Song);
+Manager.hasMany(Artist);
+Manager.hasOne(Ad);
+Ad.belongsTo(Manager);
 
 const db = <any>{};
 db.models = {
 	Artist,
 	Manager,
-	Song
+	Song,
+	Ad
 };
 
 //Export models and Sequelize for seed and dbSetup
